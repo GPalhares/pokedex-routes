e4746bee13006af4383801a5b3f9a497453f8154
Gabriel <devpalhares@gmail.com>
2022-09-30 19:33:01 -0300

    iniciand pokedex route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import './App.css';
-import pokemonList from './data';
+import { Route, Switch } from 'react-router-dom';
+import pokemonListData from './data';
 import Pokedex from './components/Pokedex';
+import About from './components/About';
 
-function App() {
-  return (
-    <div className="App">
-      <Pokedex pokemonList={ pokemonList } />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      pokemonList: pokemonListData,
+    };
+  }
+
+  render() {
+    const { pokemonList } = this.state;
+    return (
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={ () => (<Pokedex pokemonList={ pokemonList } />) }
+          />
+          <Route path="/about" component={ About } />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
